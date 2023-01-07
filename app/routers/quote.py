8b48d668e4ec3ef8482ed4edf03a34790ee298b4
Andreas Calleja <andreascalleja@gmail.com>
2023-01-07 19:25:18 +0100

    Added full quotes functionality

--- app/routers/quote.py
+++ app/routers/quote.py
@@ -1,4 +1,4 @@
-from fastapi import status, APIRouter, HTTPException, Response, Depends, Request
+from fastapi import status, APIRouter, HTTPException, Response, Depends
 from sqlalchemy.orm import Session
 from typing import List, Optional
 import random
@@ -62,3 +62,87 @@ def get_quotes(
     )
 
     return results
+
+
+@router.get("/random", response_model=schemas.QuoteResponse)
+def get_random_quote(db: Session = Depends(get_db)):
+
+    results = db.query(models.Quote).all()
+    if not results:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"No teams found",
+        )
+    quote = random.choice(results)
+
+    return quote
+
+
+@router.get("/{id}", response_model=schemas.QuoteResponse)
+def get_team(id: int, db: Session = Depends(get_db)):
+    quote = db.query(models.Quote).filter(models.Quote.id == id).first()
+
+    if not quote:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Team with id {id} was not found",
+        )
+
+    return quote
+
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT, include_in_schema=False)
+def delete_quote(
+    id: int,
+    current_user: int = Depends(oauth2.get_current_user),
+    db: Session = Depends(get_db),
+):
+
+    if current_user.id != 1:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"Unauthorized action",
+        )
+
+    quote_query = db.query(models.Quote).filter(models.Quote.id == id)
+    quote = quote_query.first()
+
+    if quote == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Team with id {id} was not found",
+        )
+
+    quote_query.delete(synchronize_session=False)
+    db.commit()
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@router.put("/{id}", response_model=schemas.QuoteResponse, include_in_schema=False)
+def update_team(
+    id: int,
+    updated_quote: schemas.QuoteCreate,
+    current_user: int = Depends(oauth2.get_current_user),
+    db: Session = Depends(get_db),
+):
+
+    if current_user.id != 1:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"Unauthorized action",
+        )
+
+    quote_query = db.query(models.Quote).filter(models.Quote.id == id)
+    quote = quote_query.first()
+
+    if quote == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Team with id {id} was not found",
+        )
+
+    quote_query.update(updated_quote.dict(), synchronize_session=False)
+    db.commit()
+
+    return quote_query.first()
