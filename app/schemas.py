e8ff98be65ccc5ac9595cb7c88bf29edd6f45359
Andreas Calleja <andreascalleja@gmail.com>
2023-01-08 20:22:41 +0100

    Removed extra imports

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,6 @@
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr
 from datetime import datetime
-from typing import Optional, Dict, List
+from typing import Optional
 
 
 class UserCreate(BaseModel):
