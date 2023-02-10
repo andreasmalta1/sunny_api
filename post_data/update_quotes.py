6fa08d9ee4e749f438d5d2575a9656e7557cd97f
Andreas Calleja <andreascalleja@gmail.com>
2023-02-10 19:12:57 +0100

    Update quote functionality on prod server

--- post_data/update_quotes.py
+++ post_data/update_quotes.py
@@ -3,26 +3,31 @@ import gspread
 from login import login
 
 
-def post_quotes():
+def update_quotes():
     sa = gspread.service_account(filename="service_account.json")
     sh = sa.open("Sunny Quotes")
-    whs = sh.worksheet("Post")
+    whs = sh.worksheet("Update")
     values = whs.get_all_values()
     header_column = values[0]
+    header_column.pop(0)
     values.pop(0)
 
     access_token = login()
     post_headers = {"Authorization": "Bearer " + access_token}
 
-    post_url = "https://itssunnyapi.com/api/quotes/"
+    base_url = "https://itssunnyapi.com/api/quotes/"
 
     for row in values:
         post_payload = {}
+        post_id = row[0]
+        update_url = base_url + post_id
+        row.pop(0)
+
         for index, column in enumerate(row):
             post_payload[header_column[index]] = column
-        response = requests.post(post_url, headers=post_headers, json=post_payload)
+        response = requests.put(update_url, headers=post_headers, json=post_payload)
 
         print(response)
 
 
-post_quotes()
+update_quotes()
